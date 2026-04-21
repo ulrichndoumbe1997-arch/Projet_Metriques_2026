96b07090e37b1cebf3d6ccf4cf4b74c74dd04978
ulrichndoumbe1997-arch <ulrichndoumbe1997@gmail.com>
2026-04-21 15:04:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ def hello_world():
     return render_template('hello.html')
 
 # Déposez votre code à partir d'ici :
-
-
-
-
-
-
-# Ne rien mettre après ce commentaire
-    
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route("/contact")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
