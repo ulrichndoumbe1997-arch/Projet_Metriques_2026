a1ff266517f1e0867db42edc0c07336ef1bab1bb
ulrichndoumbe1997-arch <ulrichndoumbe1997@gmail.com>
2026-04-21 16:04:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-import requests
-from flask import Flask, jsonify, render_template
-
-app = Flask(__name__)
-
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-# Déposez votre code à partir d'ici :
+@app.route("/contact")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
 
+@app.get("/paris")
+def api_paris():
+    # ... ton code API météo ...
+    return jsonify(result)
 
-
-
-
-
-# Ne rien mettre après ce commentaire
-    
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphique.html")
